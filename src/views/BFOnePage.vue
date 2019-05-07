0370314df968a391c2099c3bb4cf2066dc8cb799
Dracullass <ddracullass@gmail.com>
2019-05-07 11:52:16 -0300

    Parallax

--- src/views/BFOnePage.vue
+++ src/views/BFOnePage.vue
@@ -1,13 +1,49 @@
 <template>
-    <h1>This is the Battlefield One Page</h1>
+<div class="main">
+    <div class="parallax"></div>
+    <div class="info">
+        {{content}}
+    </div>
+    <div class="parallax"></div>
+</div>
+    
+    
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            content: 'Battlefield 1 é um jogo eletrônico de tiro em primeira pessoa ambientado na Primeira Guerra Mundial, desenvolvido pela EA DICE e publicada pela Electronic Arts. É o décimo quarto jogo da franquia Battlefield. Foi lançado em outubro de 2016 para Microsoft Windows, PlayStation 4 e Xbox One',
 
+        }
+    },
 }
 </script>
 
 <style>
 
+
+.parallax{
+    background-image: url('../assets/bf1bg.jpg');
+    min-height: 500px; 
+/* Create the parallax scrolling effect */
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    padding: 0;
+    margin: 0;
+}
+
+.info{
+    padding: 25px;
+    height: 200px;
+    background: black;
+    background: rgba(0, 0, 0, 0.5); /* Black see-through */
+    font-size:36px;
+    color: white;
+    justify-content: center;
+    align-items: center;    
+}
 </style>
